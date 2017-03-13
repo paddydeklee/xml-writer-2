93fe154546e5d47bdfad0c87368ea63b1c4852d9
paddydeklee <paddydeklee@gmail.com>
2017-03-13 11:43:07 +0000

    number matching plannerFolders

--- server.js
+++ server.js
@@ -1,7 +1,76 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 3000;
+// var express = require('express');
+// var app = express();
+// var PORT = process.env.PORT || 3000;
+var fs = require('fs');
+var argv = require('yargs')
+  .option('caseID', {
+        alias: 'c',
+        demand: true,
+        description: 'the case ID to be converted to XML',
+        type: 'string'
+    }).help('help')
+  .argv
 
-app.listen(PORT, function() {
-  console.log('Express listening on port ' + PORT + '!')
-})
+var plannerFolder;
+
+// read folders in planner folder
+var folders = fs.readdir('../planner', function(err, folders){
+
+    if (err){
+      throw err
+    }
+
+    console.log(folders)
+    // take the arg caseID and use it to find the correct planner folder
+    if (argv.c.length > 0) {
+      
+        console.log('case ID was provided: ' + argv.c)
+        var caseID = argv.c;
+        var matchedCase;
+
+        var targetFolder;
+
+        // read each folder within the planner folder and compare to the caseID given in args
+        folders.forEach(function(folder){
+          
+          plannerFolder = parseInt(get_numbers(folder))
+          if (plannerFolder === caseID){
+            console.log('matching ' + plannerFolder + ' with ' + caseID)
+
+            return plannerFolder
+
+          }
+
+        })
+
+      
+      } else {
+
+        console.log('CaseID was not provided')
+      }
+
+  })
+
+console.log('..reading planner folders')
+
+// get the planner folder numbers by ID
+function get_numbers(input) {
+    return input.match(/[0-9]+/g);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.listen(PORT, function() {
+//   console.log('Express listening on port ' + PORT + '!')
+// })
