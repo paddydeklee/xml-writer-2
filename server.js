85c8a17d39484ff65a486a0315ef09d1c25cdb48
paddydeklee <paddydeklee@gmail.com>
2017-03-13 09:54:49 +0000

    setting up xml writer code

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+  console.log('Express listening on port ' + PORT + '!')
+})
